291e6b7368ebf9831eb7b57f64aa18ac9243bcd8
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-24 17:07:05 -0600

    Adds zen mode

--- alt3-grid/main.js
+++ alt3-grid/main.js
@@ -83,6 +83,35 @@ const favColor = (elem) =>{
 document.querySelector("a").addEventListener("click", (e)=>{
   e.target.href= canvas.toDataURL()
 })
+
+// zen mode
+
+// Handler that uses various data-* attributes to trigger
+// specific actions, mimicing bootstraps attributes
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+window.addEventListener('click', ev => {
+  const elm = ev.target;
+  if (triggers.includes(elm)) {
+    const selector = elm.getAttribute('data-target');
+    collapse(selector, 'toggle');
+  }
+}, false);
+
+
+const fnmap = {
+  'toggle': 'toggle',
+  'show': 'add',
+  'hide': 'remove' };
+
+const collapse = (selector, cmd) => {
+  const targets = Array.from(document.querySelectorAll(selector));
+  targets.forEach(target => {
+    target.classList[fnmap[cmd]]('show');
+  });
+};
+
+// theme toggle js
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
